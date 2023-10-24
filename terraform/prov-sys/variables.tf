ccd64c2c7304061d14d7b0610a7039132d244dfb
Carlos E. Ugarte <cugarteblair@gmail.com>
2023-10-24 14:39:28 +0000

    reduce CPU/MEMORY

--- terraform/prov-sys/variables.tf
+++ terraform/prov-sys/variables.tf
@@ -116,12 +116,12 @@ variable "rds_restore_with_snapshot" {
 # Fargate
 variable "prov-sys-fargate-cpu" {
   description = "Fargate task CPU units to provision (1 vCPU = 1024 CPU units)"
-  default     = 2048
+  default     = 1024
 }
 
 variable "prov-sys-fargate-memory" {
   description = "Fargate task memory in MiB"
-  default     = 4096
+  default     = 2048
 }
 
 # containers
