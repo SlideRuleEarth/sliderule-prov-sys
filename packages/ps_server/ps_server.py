22d0609d167694e773ba5ed9a87ebfa21c64c2b4
Carlos E. Ugarte <cugarteblair@gmail.com>
2025-05-23 12:43:15 -0400

    Investigate the domain env variable in the logs #167

--- packages/ps_server/ps_server.py
+++ packages/ps_server/ps_server.py
@@ -1643,7 +1643,46 @@ class Control(ps_server_pb2_grpc.ControlServicer):
                                                 state=stateObj,
                                                 cli=clirsp)
             else:
-                yield from self.execute_sequence_of_terraform_cmds(name=request.name, ps_cmd='Refresh', cmd_args=["refresh"])                    
+                setup_cfg = read_SetUpCfg(name=request.name)
+                domain = "domain=" + get_domain_env()
+                cluster_name = "cluster_name=" + request.name  # lowercase enables matches to dns records
+                cluster_version = f"cluster_version={setup_cfg.version}"
+                node_asg_min_capacity = "node_asg_min_capacity=" + str(request.min_nodes)
+                node_asg_max_capacity = "node_asg_max_capacity=" + str(request.max_nodes)
+                node_asg_desired_capacity = "node_asg_desired_capacity=" + str(request.num_nodes)
+                spot_allocation_strategy = "spot_allocation_strategy=" + setup_cfg.spot_allocation_strategy
+                spot_max_price = "spot_max_price=" + str(setup_cfg.spot_max_price)
+                availability_zone = "availability_zone=" + setup_cfg.availability_zone
+                is_public = "is_public="+ str(setup_cfg.is_public)
+                cmd_args = [
+                    "refresh",
+                    "-var",
+                    cluster_version,
+                    "-var",
+                    domain,
+                    "-var",
+                    is_public,
+                    "-var",
+                    cluster_name,
+                    "-var",
+                    node_asg_min_capacity,
+                    "-var",
+                    node_asg_max_capacity,
+                    "-var",
+                    node_asg_desired_capacity,
+                    "-var",
+                    spot_allocation_strategy,
+                    "-var",
+                    spot_max_price,
+                    "-var",
+                    availability_zone,
+                ]
+                try:
+                    yield from self.execute_sequence_of_terraform_cmds(name=request.name, ps_cmd='Refresh', cmd_args=cmd_args)                    
+                except Exception as e:
+                    emsg = f"FAILED! {str(e)} for Update {request.name}"
+                    LOG.error(emsg)
+                    raise PS_InternalError(emsg)
         except Exception as e:
             emsg = (f" Processing Refresh {request.name} cluster caught this exception: ")
             LOG.exception(emsg)
